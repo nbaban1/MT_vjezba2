dfe4ad426e1c8c61a30cda9b79cbae1e17a498a4
Nediljko <nediljko.baban@ert.com>
2020-11-05 10:17:30 +0100

    MyData java class added

---
+++ app/src/main/java/com/example/mt_vjezba2/MyData.java
@@ -0,0 +1,13 @@
+package com.example.mt_vjezba2;
+
+public class MyData {
+    public String repoName;
+    public String starsNumber;
+    public int picture;
+
+    public MyData(String str1, String str2, int pic){
+        this.repoName = str1;
+        this.starsNumber = str2;
+        this.picture = pic;
+    }
+}
